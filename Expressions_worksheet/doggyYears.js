c007a311b04505a3cb21a34140350879830fc612
JB Elliott <jb.elliott@yahoo.com>
2013-12-04 23:41:03 -0500

    Added js for doggy years calculation v1.0

---
+++ Expressions_worksheet/doggyYears.js
@@ -0,0 +1,8 @@
+
+//Joshua Elliott
+//December 4, 2012
+//Expression Worksheet: Doggy Years
+
+var realAge = 4; //Variable to define Sparky's age in real years
+var dogAge = realAge * 7 //Variable to calculate Sparky's age in dog years
+alert ("Sparky is " + realAge + " years old which is " + dogAge + " in doggy years.");
