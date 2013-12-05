5186d5b63192df786d766e88d714ccd5ff27a638
JB Elliott <jb.elliott@yahoo.com>
2013-12-04 23:47:47 -0500

    Updated index.html title line, updated doggyYears.js to show both console and alert results. Created index2.html for Slice of Pie Part I.

--- Expressions_worksheet/doggyYears.js
+++ Expressions_worksheet/doggyYears.js
@@ -5,4 +5,5 @@
 
 var realAge = 4; //Variable to define Sparky's age in real years
 var dogAge = realAge * 7 //Variable to calculate Sparky's age in dog years
-alert ("Sparky is " + realAge + " years old which is " + dogAge + " in doggy years.");
+console.log("Sparky is " + realAge + " years old which is " + dogAge + " in doggy years."); //Console printable of result
+alert ("Sparky is " + realAge + " years old which is " + dogAge + " in doggy years."); //Alert of doggy years
