45b3bf026f039be802e7bdc413834ad9bc55f207
JB Elliott <jb.elliott@yahoo.com>
2013-12-04 23:55:18 -0500

    Added slicePartI.js, corrected index2.html to pull the script correctly.

---
+++ Expressions_worksheet/slicePartI.js
@@ -0,0 +1,13 @@
+//Joshua Elliott
+//December 4, 2013
+//Expression Worksheet: A Slice of Pie, Part I
+
+
+var partyGoers = 14;
+var slicesPerPie = 8;
+var totalPizzas = 7;
+
+var perPerson = (totalPizzas * slicesPerPie)/partyGoers;
+
+console.log("Each person at the party ate " + perPerson + " slices of pizza.");
+alert("Each person at the party ate " + perPerson + " slices of pizza.");
