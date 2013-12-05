e83941638171107b88aa23bfc1351eaeb65ab021
JB Elliott <jb.elliott@yahoo.com>
2013-12-05 00:15:31 -0500

    Created shoppingBill.js

---
+++ Expressions_worksheet/shoppingBill.js
@@ -0,0 +1,11 @@
+//Joshua Elliott
+//December 4, 2013
+//Expressions Worksheet: Average Shopping Bill
+
+var dollarsSpent = [153, 106, 137, 161, 122];
+
+var totalDollars = dollarsSpent[0] + dollarsSpent[1] + dollarsSpent[2] + dollarsSpent[3] + dollarsSpent[4];
+var avgDollars = totalDollars/5;
+
+console.log("You have spent a total of $" + totalDollars + " over the last 5 weeks. That is an average of $" + avgDollars " per week.");
+alert("You have spent a total of $" + totalDollars + " over the last 5 weeks. That is an average of $" + avgDollars " per week.");
