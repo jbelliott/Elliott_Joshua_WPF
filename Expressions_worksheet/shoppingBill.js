6e69523d0f53c1f8ebafb27fad983c7af0195de4
JB Elliott <jb.elliott@yahoo.com>
2013-12-05 00:45:37 -0500

    Created shoppingBill.js and tested.

--- Expressions_worksheet/shoppingBill.js
+++ Expressions_worksheet/shoppingBill.js
@@ -2,10 +2,12 @@
 //December 4, 2013
 //Expressions Worksheet: Average Shopping Bill
 
-var dollarsSpent = [153, 106, 137, 161, 122];
 
-var totalDollars = dollarsSpent[0] + dollarsSpent[1] + dollarsSpent[2] + dollarsSpent[3] + dollarsSpent[4];
-var avgDollars = totalDollars/5;
+var moneySpent;
+moneySpent = new Array(153, 127, 143, 140, 159);
 
-console.log("You have spent a total of $" + totalDollars + " over the last 5 weeks. That is an average of $" + avgDollars " per week.");
-alert("You have spent a total of $" + totalDollars + " over the last 5 weeks. That is an average of $" + avgDollars " per week.");
+var totalDollars = moneySpent[0] + moneySpent[1] + moneySpent[2] + moneySpent[3] + moneySpent[4];
+var avgDollars = totalDollars / 5;
+
+console.log("You have spent a total of $" + totalDollars + " on groceries over 5 weeks. That is an average of $" + avgDollars + " per week");
+alert("You have spent a total of $" + totalDollars + " on groceries over 5 weeks. That is an average of $" + avgDollars + " per week.");
