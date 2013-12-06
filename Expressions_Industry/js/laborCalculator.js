b920fe3c46f13c659d6e2117ee620a01908819f0
JB Elliott <jb.elliott@yahoo.com>
2013-12-06 00:00:44 -0500

    added variables, left space for a potential side project to assist with calculating avgHourly

--- Expressions_Industry/js/laborCalculator.js
+++ Expressions_Industry/js/laborCalculator.js
@@ -2,11 +2,14 @@
 //5 December 2013
 //Expressions:  Industry
 
-//The purpose of this script is to calculate how many crew labor hours are available for each day of the schedule
+//The purpose of this script is to calculate how many crew labor hours are available for a schedule week
 
 //This section will collect the user's data
+var forecast;
+var laborPer;
+var avgHourly;
 
-
+//possibly offer a calculation for average hourly rate??
 
 //This section will calculate the results
 
