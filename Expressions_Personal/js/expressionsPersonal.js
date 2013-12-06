1f1c1488d34faed9c5eda7a580b29eef11ea6cdf
JB Elliott <jb.elliott@yahoo.com>
2013-12-05 22:59:18 -0500

    Added basic variables and notations. Added console.log to view return result.

--- Expressions_Personal/js/expressionsPersonal.js
+++ Expressions_Personal/js/expressionsPersonal.js
@@ -2,6 +2,15 @@
 //5 December 2013
 //Expressions: Personal
 
-//This script will help me determine how many cans of energy drinks to buy for the week and take to work.
+//This script will help me determine how many cases of soda I will need to buy for my week
+
+//collect the data in these variables
+var shortDay;
+var midDay;
+var longDay;
+
+//calculate the data here
 
 
+//print to console
+console.log(totalCans);
