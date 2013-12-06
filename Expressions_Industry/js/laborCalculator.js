b5f32cf4a78d8c408165c2d7d28d1674bb2c7673
JB Elliott <jb.elliott@yahoo.com>
2013-12-06 00:58:17 -0500

    Updated comments and added comment to provide background on reason for script and usage.

--- Expressions_Industry/js/laborCalculator.js
+++ Expressions_Industry/js/laborCalculator.js
@@ -2,18 +2,22 @@
 //5 December 2013
 //Expressions:  Industry
 
+//This script is designed for my current industry--Restaurant Management
+
 //The purpose of this script is to calculate how many crew labor hours are available for a schedule week
+
 alert("The purpose of this script is to calculate how many payroll hours are available for a schedule week, rounded to the nearest whole hour.");
 
 //This section will collect the user's data
+
 var forecast = prompt("What is the sales forecast for the week \(in dollars\)?"); //Total expected sales for the week
 var laborPer = prompt("What is your target crew labor percentage?"); //Pennies per dollar allowed to be spent on crew labor
 percentage = laborPer / 100;   //convert to a decimal for percentage calculations
-
 var avgHourly = prompt("What is your store's average hourly wage? \(type 0 for help calculating\)",0); //Capture if know or redirect for help
 
 
 //possibly offer a calculation for average hourly rate??
+
 if (avgHourly = 0);
 {
   var employees = prompt("How many employees do you have?");  //Total number of crew members
@@ -24,14 +28,13 @@ if (avgHourly = 0);
 }
 
 //This section will calculate the results
+
 var availHours = forecast * percentage / avgHourly; //calculate the available number of hours
 availHours = Math.round(availHours);  //round the value to the nearest whole number to prevent partial hours
-
 
 //This section will print the results
 
 //print to console
 console.log("With a forecast of " + forecast + ", a crew percentage of " + laborPer + "%, and an average hourly rate of $" + avgHourly + ", you will have " + availHours + " to spend this week.");
-
 //print to alert
 alert("With a forecast of " + forecast + ", a crew percentage of " + laborPer + "%, and an average hourly rate of $" + avgHourly + ", you will have " + availHours + " to spend this week.");
