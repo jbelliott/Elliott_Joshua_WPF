1b56454d8037dade595848c99b7b83bf99628428
JB Elliott <jb.elliott@yahoo.com>
2013-12-05 22:47:21 -0500

    Commented name, assignment number, date, and intended use of program.

--- Expressions_Personal/js/expressionsPersonal.js
+++ Expressions_Personal/js/expressionsPersonal.js
@@ -1,3 +1,7 @@
-/**
- * Created by Prophet on 12/5/13.
- */
+//Joshua Elliott
+//5 December 2013
+//Expressions: Personal
+
+//This script will help me determine how many cans of energy drinks to buy for the week and take to work.
+
+
