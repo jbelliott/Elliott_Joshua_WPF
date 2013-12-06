b9832310e1b56b8a90073a6daaf618673bb38a65
JB Elliott <jb.elliott@yahoo.com>
2013-12-05 23:26:05 -0500

    Updated concatenating strings in console.log and alert to sound more user friendly

--- Expressions_Personal/js/expressionsPersonal.js
+++ Expressions_Personal/js/expressionsPersonal.js
@@ -23,7 +23,7 @@ alert(hoursArray[0] + hoursArray[1] + hoursArray[2]);
 var totalCans = (hoursArray[0] * shortDay) + (hoursArray[1] * midDay) + (hoursArray[2] * longDay); //Calculate how many cans of soda will be needed.
 
 //print to console
-console.log("You will need " + totalCans + " this week if you are working " + shortDay + " 4-hr shift\(s\), " + midDay + " 8-hr shift\(s\), and " + longDay + " 12-hr shift\(s\).");
+console.log("You will need " + totalCans + " of soda this week if you are working " + shortDay + " 4-hr shift\(s\), " + midDay + " 8-hr shift\(s\), and " + longDay + " 12-hr shift\(s\).");
 
 //print to alert for testing
-alert("You will need " + totalCans + " this week if you are working " + shortDay + " 4-hr shift\(s\), " + midDay + " 8-hr shift\(s\), and " + longDay + " 12-hr shift\(s\).");
+alert("You will need " + totalCans + " of soda this week if you are working " + shortDay + " 4-hr shift\(s\), " + midDay + " 8-hr shift\(s\), and " + longDay + " 12-hr shift\(s\).");
