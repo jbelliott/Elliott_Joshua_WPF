b815f775bfd7666934499d72c2ba5cc8263ab90b
JB Elliott <jb.elliott@yahoo.com>
2013-12-05 23:55:47 -0500

    Added comments to js file seperating areas of script and defining their purpose

--- Expressions_Industry/js/laborCalculator.js
+++ Expressions_Industry/js/laborCalculator.js
@@ -2,3 +2,16 @@
 //5 December 2013
 //Expressions:  Industry
 
+//The purpose of this script is to calculate how many crew labor hours are available for each day of the schedule
+
+//This section will collect the user's data
+
+
+
+//This section will calculate the results
+
+
+
+//This section will print the results
+console.log();
+alert();
