8c5c546f33deadd1340b7e7813b9af1e8130b9ee
JB Elliott <jb.elliott@yahoo.com>
2013-12-06 22:40:32 -0500

    HTML file was not referencing script correctly.  Corrected and updated.

--- Expressions_Wacky/js/numberGame.js
+++ Expressions_Wacky/js/numberGame.js
@@ -6,20 +6,17 @@
 
 
 //User data for number game
-var numberGame = new Array();
 
 alert("Now this script will guess a number you are thinking of!\nFirst, we need three numbers from you.");
+var numberGame = new Array();
 numberGame[0] = prompt("\(1\)Please select a number from 1-10");
 numberGame[1] = prompt("\(2\)Please select a number from 1-10");
 numberGame[2] = prompt("\(3\)Please select a number from 1-10");
 
 var userName = prompt("Please enter your name.");
 
-alert("Pick a number from 1-10, but keep it to yourself.\nAdd " + numberGame[0] + " to it.\nSubtract " + numberGame[1] + " from the total.\nAdd " + numberGame[2] + " to your total.\nSubtract the number you originally started with, then get ready!");
+alert("Ok, " + userName + ", pick a number from 1-10, but keep it to yourself.\nAdd " + numberGame[0] + " to it.\nSubtract " + numberGame[1] + " from the total.\nAdd " + numberGame[2] + " to your total.\nSubtract the number you originally started with, then get ready!");
 
-
-
-//Print results of mad lib
 
 
 //Print results of number game
