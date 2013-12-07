f08c7b75b169a294c0728f30889c3a579d99252f
JB Elliott <jb.elliott@yahoo.com>
2013-12-06 21:47:25 -0500

    Added comments to help organize the script

--- Expressions_Wacky/js/madLib.js
+++ Expressions_Wacky/js/madLib.js
@@ -2,4 +2,18 @@
 //6 December 2013
 //Expressions: Wacky
 
+//This script will prompt a user for words to create a mad lib, then guesses a number in your head.
+
+
+//User data for mad lib
+
+
+//User data for number game
+
+
+//Print results of mad lib
+
+
+//Print results of number game
+
 
