4a5363338105087e0d098f924e285523678d9737
JB Elliott <jb.elliott@yahoo.com>
2013-12-06 23:40:20 -0500

    Finished commenting variables and alerts

--- Expressions_Wacky/js/numberGame.js
+++ Expressions_Wacky/js/numberGame.js
@@ -7,13 +7,13 @@
 
 //User data for number game...can it guess the number you are thinking of?
 
-alert("Now this script will guess a number you are thinking of!  First, we need three numbers from you.");
-var numberGame = new Array();
-numberGame[0] = prompt("\(1\)Please select a number from 1-10");
-numberGame[1] = prompt("\(2\)Please select a number from 1-10");
-numberGame[2] = prompt("\(3\)Please select a number from 1-10");
+alert("Now this script will guess a number you are thinking of!  First, we need three numbers from you.");  //Description of script
+var numberGame = new Array();   //Create array function
+numberGame[0] = prompt("\(1\)Please select a number from 1-10");   //Part of the calculation
+numberGame[1] = prompt("\(2\)Please select a number from 1-10");   //Part of the calculation
+numberGame[2] = prompt("\(3\)Please select a number from 1-10");   //Part of the calculation
 
-var userName = prompt("Please enter your name.");
+var userName = prompt("Please enter your name.");   //Takes the name of the user to personalize the greeting
 
 alert("Ok, " + userName + ", pick a number from 1-10, but keep it to yourself.\nAdd " + numberGame[0] + " to it.\nSubtract " + numberGame[1] + " from the total.\nAdd " + numberGame[2] + " to your total.\nSubtract the number you originally started with, then get ready!"); //Alert to take user through calculation
 
