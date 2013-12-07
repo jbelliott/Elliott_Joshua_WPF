3bd117963ed1276e6ffbd79db60aa7e8ad194656
JB Elliott <jb.elliott@yahoo.com>
2013-12-06 22:08:02 -0500

    Removed mad lib to focus on number game.

--- Expressions_Wacky/js/madLib.js
+++ Expressions_Wacky/js/madLib.js
@@ -2,16 +2,7 @@
 //6 December 2013
 //Expressions: Wacky
 
-//This script will prompt a user for words to create a mad lib, then guesses a number in your head.
-
-
-//User data for mad lib
-
-alert("We're going to do a mad lib!");
-var madLibs = new Array();
-
-
-
+//This script will prompt a user for numbers, then guesses a number a user is thinking of
 
 
 //User data for number game
@@ -21,7 +12,9 @@ alert("Now this script will guess a number you are thinking of!");
 numberGame[0] = prompt("Please select a number from 1-10");
 numberGame[1] = prompt("Please select a number from 11-20");
 numberGame[2] = prompt("Please select a number from 21-30");
+numberGame[3] = Prompt("Please select a number from 31-40");
 
+a
 
 
 //Print results of mad lib
