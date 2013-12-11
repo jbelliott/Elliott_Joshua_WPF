3b9b03927335c502956b30eb5120fb7050fd696b
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 20:49:36 -0500

    Commented js file for Group 2 assignment

--- Conditionals_worksheet/js/checkLogin.js
+++ Conditionals_worksheet/js/checkLogin.js
@@ -4,20 +4,20 @@ Conditionals Worksheet: Group 2
  */
 
 //Correct username and password
-var correctUsername = "drwho";
-var correctPass = "tardis123";
+var correctUsername = "drwho"; //given username
+var correctPass = "tardis123"; //given password
 
 //Prompt for username and password
 
-var enterUsername = prompt("Welcome to the U.N.I.T. login page. Please enter your username.");
-var enterPW = prompt("Please enter your password.");
+var enterUsername = prompt("Welcome to the U.N.I.T. login page. Please enter your username.");  //user entered username
+var enterPW = prompt("Please enter your password.");   ///user entered password
 
 //Conditional test of user information
 
-if (enterUsername === correctUsername && enterPW === correctPass){
-    console.log("Welcome, " + correctUsername + "!");
-}else if(enterUsername === correctUsername){
-    console.log("Password does not match our records.");
+if (enterUsername === correctUsername && enterPW === correctPass){ //Conditional test
+    console.log("Welcome, " + correctUsername + "!");  //Response if both are true
+}else if(enterUsername === correctUsername){  //Test against only username
+    console.log("Password does not match our records.");  //Password is wrong if username correct
 }else{
-    console.log("User not found. Try again.");
+    console.log("User not found. Try again."); //Otherwise, username was incorrect to cause first test to fail
 }
