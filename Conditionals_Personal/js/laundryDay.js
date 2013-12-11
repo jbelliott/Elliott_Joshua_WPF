768164507b1f7f96edc619e86b869424e9262e38
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 22:42:41 -0500

    Created second user prompt and conditional to check user input.

--- Conditionals_Personal/js/laundryDay.js
+++ Conditionals_Personal/js/laundryDay.js
@@ -12,10 +12,17 @@ if (numberOutfits == ""){
     numberOutfits = prompt("Invalid entry. Please enter how many outfits you have for work?",0);
     if (numberOutfits == ""){
         alert("We'll assume your hesitation to answer means 0.");
+        numberOutfits = 0;
     }
 }
 var numberShifts = prompt("How many shifts do you have before your next day off?");
-
+if (numberShifts == ""){
+    numberShifts = prompt("Invalid entry. Please enter how many shifts you have this week?",0);
+    if (numberShifts == ""){
+        alert("Not working this week, eh? 0 shifts it is.");
+        numberShifts = 0;
+    }
+}
 
 
 //Calculation to determine if user has enough outfits
