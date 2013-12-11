4d014bd50b2f115a17b85241a1444142b2d285a5
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 20:17:07 -0500

    Added variables and prompts to collect data to make script reusable without having to change scripting code.

--- Conditionals_worksheet/js/gasCheck.js
+++ Conditionals_worksheet/js/gasCheck.js
@@ -3,3 +3,11 @@
 Conditionals Worksheet: Group 1
  */
 
+//Return "Yes, you can make it without stopping for gas! or You only have x gallons of gas in your tank, better get gas now while you can!
+//This will calculate if you should get gas now or if you can make it 200 miles to the next station.
+
+//Variables collected to make script more reusable
+
+var fuelEfficiency = prompt("How many miles do you get per gallon of gas?");
+var fuelLevel = prompt("How full is the tank now (percentage)?");
+var fuelCapacity = prompt("How many gallons of fuel can your tank hold?");
