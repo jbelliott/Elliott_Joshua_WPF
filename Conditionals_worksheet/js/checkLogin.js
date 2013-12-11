bebbe2cdfd1e55dda93da02e840d82e9c71d97fb
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 20:32:55 -0500

    Created js file and setup the information needed to solve the problem from group 2

---
+++ Conditionals_worksheet/js/checkLogin.js
@@ -0,0 +1,9 @@
+/*Joshua Elliott
+10 December, 2013
+Conditionals Worksheet: Group 2
+ */
+
+//username, pw, correct user, correct pw
+
+
+//welcome, username!   user not found. try again. password does not match our records
