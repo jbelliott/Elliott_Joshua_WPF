f3bee95a8c5d2f4b45065060f28e7cb1ae9c7039
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 22:19:28 -0500

    Created js file, linked to html document. Comments setup to determine flow of script.

---
+++ Conditionals_Personal/js/laundryDay.js
@@ -0,0 +1,15 @@
+/*Joshua Elliott
+10 December 2013
+Conditionals: Personal
+ */
+
+//This script will gather information on how many work outfits a user has and will return if they need to do laundry today
+
+
+//Define and prompt user for information: number of outfits, number of shifts
+
+
+//Basic conditional argument to determine if the user needs to do laundry
+
+
+//Return result
