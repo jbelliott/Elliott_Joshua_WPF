d285136ce8597a320bd16b7b2dbfb2ffc63eaad8
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 20:27:13 -0500

    Added conditional statement.  Tested out correctly.

--- Conditionals_worksheet/js/gasCheck.js
+++ Conditionals_worksheet/js/gasCheck.js
@@ -11,3 +11,12 @@ Conditionals Worksheet: Group 1
 var fuelEfficiency = prompt("How many miles do you get per gallon of gas?");
 var fuelLevel = prompt("How full is the tank now (percentage)?");
 var fuelCapacity = prompt("How many gallons of fuel can your tank hold?");
+
+//Calculate if gas should be gotten now
+
+var currentLevel = fuelLevel/100 * fuelCapacity;
+if (currentLevel * fuelEfficiency >= 200){
+    console.log("Yes, you can make it without stopping for gas!");
+}else{
+    console.log("You only have " + currentLevel + " gallons of gas in your tank, better get gas now while you can!");
+}
