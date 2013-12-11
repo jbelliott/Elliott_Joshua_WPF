58ed6e12ba2900dad6002eaff9b2f787fa454371
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 23:23:59 -0500

    Added conditional statement framework and declared variables.

--- Conditionals_Industry/js/foodCost.js
+++ Conditionals_Industry/js/foodCost.js
@@ -6,10 +6,19 @@ Conditionals: Industry
 //This script will help determine if your restaurant is on target with your food costs.  On target is within 1% of net sales EITHER positive or negative
 
 //Collect variables:  Net Sales, starting inventory dollars, closing inventory dollars, cost of sales target %
-
+var netSales;
+var inventoryOpen;
+var inventoryClose;
+var cosPercent = 1;
 
 
 //Calculate Cost of Sales to compare if within allowable variance
-
+var salesCost = inventoryOpen - inventoryClose;
+var cosTarget = netsales * (cosPercent/100);
 
 //Conditionals to determine if CoS is within variance
+if (salesCost/netSales < cosTarget){
+    alert("Your food cost is on target. Good job!");
+}else{
+    alert("Your food cost is out by " + Math.abs(cosTarget - salesCost));
+}
