b0542f68e8d8a83773ad4d5ef94f39b70819b36e
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 20:52:19 -0500

    Commented js file for Group 1

--- Conditionals_worksheet/js/gasCheck.js
+++ Conditionals_worksheet/js/gasCheck.js
@@ -3,20 +3,17 @@
 Conditionals Worksheet: Group 1
  */
 
-//Return "Yes, you can make it without stopping for gas! or You only have x gallons of gas in your tank, better get gas now while you can!
-//This will calculate if you should get gas now or if you can make it 200 miles to the next station.
-
 //Variables collected to make script more reusable
 
-var fuelEfficiency = prompt("How many miles do you get per gallon of gas?");
-var fuelLevel = prompt("How full is the tank now (percentage)?");
-var fuelCapacity = prompt("How many gallons of fuel can your tank hold?");
+var fuelEfficiency = prompt("How many miles do you get per gallon of gas?");   //User prompt to establish efficiency
+var fuelLevel = prompt("How full is the tank now (percentage)?");   //Percentage of tank full
+var fuelCapacity = prompt("How many gallons of fuel can your tank hold?");   //Capacity of tank
 
 //Calculate if gas should be gotten now
 
-var currentLevel = fuelLevel/100 * fuelCapacity;
-if (currentLevel * fuelEfficiency >= 200){
-    console.log("Yes, you can make it without stopping for gas!");
+var currentLevel = fuelLevel/100 * fuelCapacity;  //Convert to how many gallons are in fuel tank
+if (currentLevel * fuelEfficiency >= 200){   //Test if car will travel 200 miles before running out of gas
+    console.log("Yes, you can make it without stopping for gas!");  //Response if test is true
 }else{
     console.log("You only have " + currentLevel + " gallons of gas in your tank, better get gas now while you can!");
-}
+} //Response if test is false
