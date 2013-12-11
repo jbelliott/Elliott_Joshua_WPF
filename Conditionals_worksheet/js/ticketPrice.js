effac6b6de1bc144d7e4c67f8be37d3e89e439c0
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 20:58:05 -0500

    Created js file for Group 3 conditional worksheet assignment.

---
+++ Conditionals_worksheet/js/ticketPrice.js
@@ -0,0 +1,10 @@
+/*Joshua Elliott
+10 December, 2013
+Conditionals Worksheet: Group 3
+ */
+
+//Ticket price is 12$ or 7$ for age 55+ or age < 10 or between 3pm or 5pm
+
+//Given age and time of movie
+
+//Return cost of ticket based on information
