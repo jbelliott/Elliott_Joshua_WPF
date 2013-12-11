e9572e9bd07356da19b8b77804dc9d39dbf130f4
JB Elliott <jb.elliott@yahoo.com>
2013-12-10 23:05:55 -0500

    Created js file and linked to index.html for access.  Setup comments to determine flow of the script and reasoning for its use.

---
+++ Conditionals_Industry/js/foodCost.js
@@ -0,0 +1,15 @@
+/*Joshua Elliott
+10 December 2013
+Conditionals: Industry
+ */
+
+//This script will help determine if your restaurant is on target with your food costs.  On target is within 1% of net sales EITHER positive or negative
+
+//Collect variables:  Net Sales, starting inventory dollars, closing inventory dollars, cost of sales target %
+
+
+
+//Calculate Cost of Sales to compare if within allowable variance
+
+
+//Conditionals to determine if CoS is within variance
