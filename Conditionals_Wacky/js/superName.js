8def73c62eb1cb74f1e50baa60ccd7e71c46d4b3
JB Elliott <jb.elliott@yahoo.com>
2013-12-11 01:43:47 -0500

    Corrected issue with else if statements reading incorrectly.

--- Conditionals_Wacky/js/superName.js
+++ Conditionals_Wacky/js/superName.js
@@ -6,6 +6,7 @@ Conditionals: Wacky
 //This makes user input into a comic book character name
 
 //Collect user data:
+var nameNoun;
 var whichSide;
 var goodBad = prompt("Are you a hero or villain?");
 if (goodBad == ""){
@@ -13,6 +14,7 @@ if (goodBad == ""){
     whichSide = 1;
 }else if(goodBad === "hero"){
     whichSide = 1;
+    alert("You are a hero.");
 }else{
     whichSide = 2;
     alert("You are a villain.");
@@ -22,11 +24,13 @@ if (randomNumber == ""){
     randomNumber = prompt("Last chance to pick a number.");
     if (randomNumber == ""){
         alert("Your number is 3.");
+        randomNumber = 3;
     }
-}
-if (randomNumber <= 0 || randomNumber > 5){
+}else{
+    if(randomNumber <= 0 || randomNumber > 5){
     alert("Number out of range. You have been assigned the number 4.");
     randomNumber = 4;
+}
 }
 var adjective = prompt("Type an adjective to describe your character.");
 if (adjective == ""){
@@ -41,22 +45,16 @@ if (adjective == ""){
         }
     }
 }
-switch (randomNumber){
-    case 1:
-        nameNoun = "chicken";
-        break
-    case 2:
-        nameNoun = "swan";
-        break
-    case 3:
-        nameNoun = "mongoose";
-        break
-    case 4:
-        nameNoun = "badger";
-        break
-    default:
-        nameNoun = "lion";
-        break
+if (randomNumber == "5"){
+    nameNoun = "chicken";
+}else if(randomNumber == "4"){
+    nameNoun = "swan";
+}else if(randomNumber == "3"){
+    nameNoun = "mongoose";
+}else if (randomNumber == "2"){
+    nameNoun = "badger";
+}else{
+    nameNoun = "lion";
 }
 
-alert("Your comic book name is \"The " + adjective + " " + nameNoun + "\"");
+console.log("Your comic book name is The " + adjective + " " + nameNoun);
