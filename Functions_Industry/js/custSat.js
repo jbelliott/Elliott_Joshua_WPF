279937fa5adb7e3d6749c869fcba52d44ec4d70a
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 22:27:45 -0500

    Updated checks to set points at a maximum of 10.

--- Functions_Industry/js/custSat.js
+++ Functions_Industry/js/custSat.js
@@ -7,28 +7,40 @@ Functions: Industry
 //This script will help calculate customer satisfaction ratings in 3 categories
 
 //User input for results provided
-var scoreAccuracy = prompt("Please enter your accuracy score (0-30)");
+var scoreAccuracy = prompt("Please enter your accuracy score (0-10)");
 if (scoreAccuracy < 0){
     scoreAccuracy = 0;
-}else if (scoreAccuracy > 30){
-    scoreAccuracy = prompt("Value outside of the range. Please re-enter (0-30)");
-    if (scoreAccuracy > 30){
-        scoreAccuracy = 30;
+}else if (scoreAccuracy > 10){
+    scoreAccuracy = prompt("Value outside of the range. Please re-enter (0-10)", 10);
+    if (scoreAccuracy > 10){
+        scoreAccuracy = 10;
     }
 }
 
-var scoreQuality = prompt("Please enter your quality score (0-30)");
+var scoreQuality = prompt("Please enter your quality score (0-10)");
 if (scoreQuality < 0){
     scoreQuality = 0;
-}else if (scoreQuality > 30){
-    scoreQuality = prompt("Value outside of the range. Please re-enter (0-30)");
-    if (scoreQuality > 30){
-        scoreQuality = 30;
+}else if (scoreQuality > 10){
+    scoreQuality = prompt("Value outside of the range. Please re-enter (0-10)", 10);
+    if (scoreQuality > 10){
+        scoreQuality = 10;
     }
 }
 
+var scoreHospitality = prompt("Please enter your hospitality score (0-10)");
+if (scoreHospitality < 0){
+    scoreHospitality = 0;
+}else if (scoreHospitality > 10){
+    scoreHospitality = prompt("Value outside of the range. Please re-enter (0-10)", 10);
+    if (scoreHospitality > 10){
+        scoreHospitality = 10;
+    }
+}
 
-//Static values for score targets
-
+//Function to calculate scores
+function guestCheck(acc, qual, hosp){
+    var totalScore = Number(acc) + Number(qual) + Number(hosp);
+    return totalScore;
+}
 
 //
