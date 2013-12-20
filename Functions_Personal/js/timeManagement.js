31175fc1395aeb2e2003b6ab4823169ec7854104
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 20:35:25 -0500

    Created js file comments to organize project

--- Functions_Personal/js/timeManagement.js
+++ Functions_Personal/js/timeManagement.js
@@ -3,3 +3,24 @@ Joshua Elliott
 19 December 2013
 Functions: Personal
  */
+
+//This script will calculate how much free time (if any) based on user inputs
+
+
+//Collect variables from user
+
+
+
+//Define function
+
+
+
+//Call function
+
+
+
+//Conditional statement to determine if user will have free time this week
+
+
+
+//Return result to user
