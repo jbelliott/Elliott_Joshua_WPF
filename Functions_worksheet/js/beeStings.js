4353b967ad22537bb169c781fbd7caa05a83bd0f
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 19:59:26 -0500

    Created comments to organize js file.

--- Functions_worksheet/js/beeStings.js
+++ Functions_worksheet/js/beeStings.js
@@ -2,3 +2,16 @@
 19 December 2013
 Functions Worksheet: Bee Stings
  */
+
+//Given that it takes 8.666666667 bee stings per pound to kill an animal...
+
+//Collect the weight of the victim being stung
+
+
+//Function created to calculate and return the number of stings
+
+
+//Call function for use
+
+
+//Return result to user
