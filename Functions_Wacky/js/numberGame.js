1e64cc795cf29dad076cfb4573f5922af72ab997
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 23:17:21 -0500

    Commented to setup outline for script

--- Functions_Wacky/js/numberGame.js
+++ Functions_Wacky/js/numberGame.js
@@ -4,5 +4,12 @@ Joshua Elliott
 Functions: Wacky
  */
 
-//This is another type of number game to predict what a user is thinking
+//This is another type of number game to predict a user's age based on input
 
+//Explain the game to the user
+
+
+//Collect user input
+
+
+//Function to calculate the
