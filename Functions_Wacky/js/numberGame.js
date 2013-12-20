599703b82d618af5f458ac24f2ea13ddf8c9c3ad
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 23:28:18 -0500

    Created function to calculate age based on user input.

--- Functions_Wacky/js/numberGame.js
+++ Functions_Wacky/js/numberGame.js
@@ -7,9 +7,18 @@ Functions: Wacky
 //This is another type of number game to predict a user's age based on input
 
 //Explain the game to the user
-
+var gameMechanics = function(){
+    alert("Take your age")
+}
 
 //Collect user input
+var numberTotal = prompt("Test");
 
+//Function to calculate the user's age
+function calcAge(ageTotal){
+    var userAge = ageTotal - 6;
+    return userAge;
+}
+var userAge = calcAge(numberTotal);
 
-//Function to calculate the
+alert(userAge);
