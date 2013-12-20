fd579304d94d096f4e3792d294f377130cb0b238
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 21:05:06 -0500

    Created function to add totals together for hours needed for their week.

--- Functions_Personal/js/timeManagement.js
+++ Functions_Personal/js/timeManagement.js
@@ -11,18 +11,19 @@ Functions: Personal
 var hoursSleep = prompt("How many hours a night do you sleep?");
 var hoursWork = prompt("How many hours are you working this week?");
 var hoursSchool = prompt("How many hours of schoolwork do you have this week?");
-
+var hoursWeek = 168;
 
 //Define function
-
-
+function calcHours(hrsSleep, hrsWork, hrsSchool){
+    var totalHrs = hrsSleep + hrsWork + hrsSchool;
+    return totalHrs;
+}
 
 //Call function
-
-
+var hoursUsed = calcHours(hoursSleep, hoursWork, hoursSchool);
 
 //Conditional statement to determine if user will have free time this week
-
+if 
 
 
 //Return result to user
