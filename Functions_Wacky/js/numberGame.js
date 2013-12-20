3eb34ffabdd04c44bd43d9593e56288db87422cc
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 23:35:14 -0500

    Added prompt to gather user's calculated total for processing.

--- Functions_Wacky/js/numberGame.js
+++ Functions_Wacky/js/numberGame.js
@@ -14,7 +14,7 @@ var gameMechanics = function(){
 gameMechanics();
 
 //Collect user input
-var numberTotal = prompt("Test");
+var numberTotal = prompt("Finally, add the second number in your age to the total and type it below. \nExample: If you are 32, you would add 2 to your running total");
 
 //Function to calculate the user's age
 function calcAge(ageTotal){
