a19c780cb491691c0658b3438da841d2aa25bc5f
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 20:02:18 -0500

    Created prompt for user information, outlined the function to use, created a call function setting up a return result.

--- Functions_worksheet/js/beeStings.js
+++ Functions_worksheet/js/beeStings.js
@@ -6,12 +6,15 @@ Functions Worksheet: Bee Stings
 //Given that it takes 8.666666667 bee stings per pound to kill an animal...
 
 //Collect the weight of the victim being stung
-
+var victimWeight = prompt("Enter the weight of the animal victim being stung.");
 
 //Function created to calculate and return the number of stings
-
+function beeStings(victimLbs){
+    //code to calculate
+}
 
 //Call function for use
-
+var stingTotal = beeStings(victimWeight);
 
 //Return result to user
+console.log("It will take " + stingTotal + " bee stings to kill this animal.");
