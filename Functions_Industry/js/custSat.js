709584548ee1b24e42ae79b2e0c3bb0597180b80
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 22:19:31 -0500

    Created data capture for accuracy score variable with conditionals that check for its range.

--- Functions_Industry/js/custSat.js
+++ Functions_Industry/js/custSat.js
@@ -4,3 +4,21 @@ Joshua Elliott
 Functions: Industry
  */
 
+//This script will help calculate customer satisfaction ratings in 3 categories
+
+//User input for results provided
+var scoreAccuracy = prompt("Please enter your accuracy score (0-30)");
+if (scoreAccuracy < 0){
+    scoreAccuracy = 0;
+}else if (scoreAccuracy > 30){
+    scoreAccuracy = prompt("Value outside of the range. Please re-enter (0-30)");
+    if (scoreAccuracy > 30){
+        scoreAccuracy = 30;
+    }
+}
+
+
+//Static values for score targets
+
+
+//
