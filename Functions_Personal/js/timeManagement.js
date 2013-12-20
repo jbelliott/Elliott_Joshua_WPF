0a9528d771a94ad2f5708f4361881b4f0478268a
JB Elliott <jb.elliott@yahoo.com>
2013-12-19 20:46:27 -0500

    Created user input variables

--- Functions_Personal/js/timeManagement.js
+++ Functions_Personal/js/timeManagement.js
@@ -4,11 +4,13 @@ Joshua Elliott
 Functions: Personal
  */
 
-//This script will calculate how much free time (if any) based on user inputs
+//This script will calculate how much free time (if any) based on user inputs.
 
 
-//Collect variables from user
-
+//Collect variables from user to subtract from the number of hours in a week
+var hoursSleep = prompt("How many hours a night do you sleep?");
+var hoursWork = prompt("How many hours are you working this week?");
+var hoursSchool = prompt("How many hours of schoolwork do you have this week?");
 
 
 //Define function
